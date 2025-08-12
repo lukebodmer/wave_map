408325f6f4944b2c196367b80e1ffd18880ea2c8
lukebodmer <lukejbodmer@gmail.com>
2025-08-12 17:47:13 -0400

    reorganized file structure

--- scripts/run_gui.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
-
-from gui.user_interface import UserInterface 
-
-
-ui = UserInterface(outputs_dir='data/outputs')
-ui.show().servable()
